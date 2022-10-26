294aad03d7e9c53b01ce47a17a14ffee6eced819
FMazmi <azmi.elfaruk@gmail.com>
2022-10-26 07:22:28 +0700

    updaet

--- product_book.js
+++ product_book.js
@@ -2,6 +2,8 @@
  * @param {Book} book
  * @param {Number} discount
  * @param {Number} tax
+ * @param {Number} stock
+ * @param {Number} totalOrder
  */
 
 function countPrice(book, discount, tax){
@@ -16,6 +18,7 @@ function countPrice(book, discount, tax){
         
         console.group();
         console.log('------------------------------------')
+        console.log(book.tittle);
         console.log('Harga Asli \t\t: Rp.', price);
         console.log('Total Diskon \t\t: Rp.', totaldiscount);
         console.log('Harga setelah Diskon: Rp.', priceAfterDiscount);
@@ -26,6 +29,6 @@ function countPrice(book, discount, tax){
     }
     return 0;
 }
-countPrice({tittle: "Album 1", price:5000, publish: true}, 50, 10);
-countPrice({tittle: "Album 2", price:10000, publish: false}, 50, 10);
-countPrice({tittle: "Album 3", price:15000, publish: true}, 50, 10);
+countPrice({tittle: "Album Redvelvet", price:5000, publish: true}, 50, 10);
+countPrice({tittle: "Album Twice", price:10000, publish: false}, 50, 10);
+countPrice({tittle: "Album NCT", price:15000, publish: true}, 50, 10);
