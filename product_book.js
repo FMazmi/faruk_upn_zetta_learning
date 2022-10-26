bb595ce27a8d05588293698ea72064110d22da00
FMazmi <azmi.elfaruk@gmail.com>
2022-10-27 05:49:22 +0700

    update

--- product_book.js
+++ product_book.js
@@ -2,30 +2,53 @@
  * @param {Book} book
  * @param {Number} discount
  * @param {Number} tax
+ * @param {Number} stock
+ * @param {Number} totalOrder
  */
 
-function countPrice(book, discount, tax){
-    let result = 0;
+ function countPrice(book, discount, tax, stock, totalOrder){
+    let totalPay = 0;
+    let totalPrice = 0;
+    let totalAfterDiscount = 0;
+    let totalTax = 0;
+    
+    const price = book.price;
+    const totaldiscount = price * (discount/100);
+    const priceAfterDiscount = price - totaldiscount;
+    const priceTax = priceAfterDiscount * (tax/100);
+    const priceAfterTax = priceAfterDiscount + priceTax;
+
+    for(let index = 0; index < totalOrder; index++){
+        if(book && book.price && (index < stock)){
+            totalPrice += book.price;
+            totalAfterDiscount += priceAfterDiscount;
+            totalTax += priceTax;
+        }
+        else{
+            console.log('Stock has run Out!');
+            break;
+        }
+    }
+    totalPay = totalAfterDiscount + totalTax;
     if(book && book.price){
-        const price = book.price;
-        const totaldiscount = price * (discount/100);
-        const priceAfterDiscount = price - totaldiscount;
-        const totaltax = priceAfterDiscount * (tax/100);
-        const priceAfterTax = priceAfterDiscount + totaltax;
-        result += priceAfterTax;
         
         console.group();
-        console.log('------------------------------------')
-        console.log('Harga Asli \t\t: Rp.', price);
-        console.log('Total Diskon \t\t: Rp.', totaldiscount);
-        console.log('Harga setelah Diskon: Rp.', priceAfterDiscount);
-        console.log('Total Pajak \t\t: Rp.', totaltax);
-        console.log('Total setelah Pajak: Rp.', priceAfterTax)
-        console.log()
+        console.log('------------------------------------');
+        console.log(book.tittle);
+        console.log('Harga Satuan \t\t: Rp.',price);
+        console.log('Jumlah Order \t\t:', totalOrder);
+        console.log('Total Harga \t\t: Rp.',totalPrice)
+        console.log('Total Diskon \t\t: Rp.',totaldiscount);
+        console.log('Harga setelah Diskon: Rp.',totalAfterDiscount);
+        console.log('Biaya Pajak \t\t: Rp.',totalTax)
+        console.log('Total Pembayaran \t: Rp.',totalPay)
+        console.log('====================================');
+        console.log('Stok yang tersedia:', stock - totalOrder);
+        console.log();
         console.groupEnd();
     }
-    return 0;
+    return totalPay;
 }
-countPrice({tittle: "Album 1", price:5000, publish: true}, 50, 10);
-countPrice({tittle: "Album 2", price:10000, publish: false}, 50, 10);
-countPrice({tittle: "Album 3", price:15000, publish: true}, 50, 10);
+countPrice({tittle: "Album RedVelvet", price:5000, publish: true}, 50, 10, 10, 8);
+countPrice({tittle: "Album Twice", price:10000, publish: false}, 50, 10, 5, 5);
+countPrice({tittle: "Album NCT", price:15000, publish: true}, 50, 10, 5, 0);
