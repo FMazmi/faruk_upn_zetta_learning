71b9929b828589ca00464a43b1ca9bd7f8e31529
FMazmi <azmi.elfaruk@gmail.com>
2022-10-27 15:17:43 +0700

    add credit

--- product_book.js
+++ product_book.js
@@ -4,19 +4,20 @@
  * @param {Number} tax
  * @param {Number} stock
  * @param {Number} totalOrder
+ * @param {Number} credit
  */
 
- function countPrice(book, discount, tax, stock, totalOrder){
+ function countPrice(book, discount, tax, stock, totalOrder, credit){
     let totalPay = 0;
     let totalPrice = 0;
     let totalAfterDiscount = 0;
     let totalTax = 0;
+    let priceInstallment = 0;
     
     const price = book.price;
     const totaldiscount = price * (discount/100);
     const priceAfterDiscount = price - totaldiscount;
     const priceTax = priceAfterDiscount * (tax/100);
-    const priceAfterTax = priceAfterDiscount + priceTax;
 
     for(let index = 0; index < totalOrder; index++){
         if(book && book.price && (index < stock)){
@@ -39,16 +40,27 @@
         console.log('Jumlah Order \t\t:', totalOrder);
         console.log('Total Harga \t\t: Rp.',totalPrice)
         console.log('Total Diskon \t\t: Rp.',totaldiscount);
-        console.log('Harga setelah Diskon: Rp.',totalAfterDiscount);
+        console.log('Harga setelah Diskon\t: Rp.',totalAfterDiscount);
         console.log('Biaya Pajak \t\t: Rp.',totalTax)
         console.log('Total Pembayaran \t: Rp.',totalPay)
         console.log('====================================');
         console.log('Stok yang tersedia:', stock - totalOrder);
         console.log();
+        console.log('=> Anda melakukan cicilan sebanyak:', credit, 'kali')
+        console.log();
         console.groupEnd();
     }
+    priceInstallment = totalPay / credit;
+    let installment = [];
+    for (let index = 0; index < credit; index++){
+        installment.push({
+            Angsuran_Bulan_Ke: index + 1,
+            Rp: priceInstallment
+        })
+    }
+    console.log(Array.from(installment));
     return totalPay;
 }
-countPrice({tittle: "Album RedVelvet", price:5000, publish: true}, 50, 10, 10, 8);
-countPrice({tittle: "Album Twice", price:10000, publish: false}, 50, 10, 5, 5);
-countPrice({tittle: "Album NCT", price:15000, publish: true}, 50, 10, 5, 0);
+countPrice({tittle: "Album RedVelvet", price:5000000, publish: true}, 20, 10, 10, 8, 12);
+countPrice({tittle: "Album Twice", price:1000000, publish: false}, 20, 10, 5, 5, 12);
+countPrice({tittle: "Album NCT", price:1500000, publish: true}, 20, 10, 5, 0, 0);
