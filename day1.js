ae70baa5b11a2f939d97197391b3c42e45beb89a
FMazmi <azmi.elfaruk@gmail.com>
2022-10-28 15:12:50 +0700

    ts - commit

---
+++ day1.js
@@ -0,0 +1,7 @@
+function returnText(text, index1, index2) {
+    return text;
+}
+var text = "Learning Typescript is different than Javasrcipt";
+var index1 = 9;
+var index2 = 19;
+console.log(returnText(text, index1, index2));
---
+++ day1.ts
@@ -0,0 +1,8 @@
+function returnText(text: string, index1: number, index2: number):string{
+    return text;
+}
+const text = "Learning Typescript is different than Javasrcipt";
+const index1 = 9;
+const index2 = 19;
+
+console.log(returnText(text, index1, index2));
